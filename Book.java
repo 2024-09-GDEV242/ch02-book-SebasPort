804137f29bb7f8233879696629b3604bc589b79f
SebasPort <G00306331@stu.raritanval.edu>
2024-09-17 17:40:44 -0400

    Book chptr2 code
    For this project, I am adding a buch of features into the book project, however some changes may come in the colse future

--- Book.java
+++ Book.java
@@ -11,16 +11,79 @@ class Book
     // The fields.
     private String author;
     private String title;
-
+    private int pages;
+    private String details; 
+    private String refnumbers = "";
+    private int borrowed;
+    private boolean courseText;
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages, String bookRefnumber, int bookBorrowed, boolean isCourseText)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
+        refnumbers = bookRefnumber;
+        borrowed = bookBorrowed;
+        courseText = isCourseText;
+        
+        
+        
+        
+    }
+    
+     public String getAuthor(){
+        return author;
+     }
+     public String getTitle(){
+         return title;
+     }
+     public int getPages(){
+         return pages;
+     }
+     public String printAuthor(){
+         System.out.println( author );
+         return author;
+     }
+     public String printTitle(){
+         System.out.println( title );
+         return title;
+     }
+     public String printDetails(){
+         System.out.println("Title: " + title+"," + " Author:" + author+"," + " Pages:" + pages);
+         System.out.println("Number of times borrowed: "+ borrowed);
+         System.out.println("Book is course text: " + courseText);
+             if (refnumbers.length() > 0){
+             System.out.println("Reference number:" + refnumbers);
+             
+            }
+            else {
+                System.out.println("Reference number: ZZZ");
+            }
+         return details;
+     }
+     public void setRefNumber(String ref){
+         if (refnumbers.length() > 3){
+             ref = refnumbers;
+          }
+        else {
+         System.out.println("ERROR: REFERENCE NUMBER MUST CONTAIN ATLEAST 3 CHARACTERS");
+         }
     }
 
+    public String getRefNumber(){
+          return refnumbers;
+     }
+    public void borrowed(){
+         borrowed++;
+     }
+    public int getBorrowed(){
+        return borrowed;
+        }
+    public Boolean iscourseText(){
+        return courseText;
+    }
     // Add the methods here ...
 }
